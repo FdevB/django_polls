0b7aa0b529c92f0459130a16329034748b67a06c
FarhamB.dev <165949272+FdevB@users.noreply.github.com>
2025-07-25 14:47:37 +0430

    feat (polls_app): create better models
    - add DocStrings
    - edit admin for  QuestionModel
    - add updated_at, published_at , slug fields
    - create logic for slug and set published_at & status (semi-automatic)

--- polls_app/admin.py
+++ polls_app/admin.py
@@ -5,7 +5,21 @@ from polls_app import models
 # Register your models here.
 @admin.register(models.QuestionModel)
 class QuestionAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the PostModel.
+
+    This class registers PostModel in the Django admin interface with all its features.
+
+    Attributes:
+        date_hierarchy (str): Enables date-based navigation by the specified date field.
+        list_display (list[str]): Fields to display as columns in the list view.
+        list_filter (list[str]): Fields to filter by in the sidebar.
+        search_fields (list[str]): Fields that are searchable in the admin.
+        readonly_fields (list[str]): Fields set as read-only in the admin form.
+    """
+
     date_hierarchy = "created_at"
     list_display = ['question_text', 'status', 'created_at']
-    search_fields = ['question_text']
     list_filter = ['status']
+    search_fields = ['question_text']
+    readonly_fields = ['slug']
--- polls_app/models.py
+++ polls_app/models.py
@@ -1,28 +1,77 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.utils import timezone
 
 # Create your models here.
 class QuestionModel(models.Model):
+    """
+    Model definition for QuestionModel.
+
+    This class defines the QuestionModel database schema and its related behaviors.
+
+    Attributes:
+        author (ForeignKey to User): Author of the post.
+        question_text (CharField): Question text of the question.
+        STATUS_CHOICES (variable): All choices for status.
+        status (CharField): Choose status of the post (published or draft) from STATUS_CHOICES.
+        created_at (DateField): Date when the post was created.
+        updated_at (DateField): Date when the post was last updated.
+        published_at (DateTimeField): Date and time when the post was published.
+        slug (SlugField): Slug derived from the title (non-editable).
+    """
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='question')
     question_text = models.CharField(max_length=250)
     
-    choice = (
+    STATUS_CHOICES = (
         ('pub', 'published'),
         ('dra', 'draft')
     )
-    status = models.CharField(max_length=3, choices=choice)
+    status = models.CharField(max_length=3, choices=STATUS_CHOICES, default='draft')
 
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+
+    slug = models.SlugField(unique=True)
 
     class Meta:
         verbose_name = 'Question'
 
     def __str__(self):
         return self.question_text
+    
+    def save(self, *args, **kwargs):
+        """
+        Overrides the default save method to automatically set the slug field.
+        """ 
+
+        self.slug = slugify(self.slug)
+
+        if (self.status == 'published') and (self.published_at is None):
+            self.published_at = timezone.now()
+        
+        elif (self.published_at is not None) and (timezone.now() >= self.published_at):
+            self.status = 'published'
+
+        else:
+            self.status = 'draft'
 
 
 class AnswerModel(models.Model):
+    """
+    Model definition for AnswerModel.
+
+    This class defines the AnswerModel database schema and its related behaviors.
+
+    Attributes:
+        author (ForeignKey to User): Author of the post.
+        question (ForeignKey to QuestionModel): Answer to this question.
+        choice_text (CharField): Answer text.
+        votes (IntegerField): Count of votes.
+    """
+
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answer')
     question = models.ForeignKey(QuestionModel, on_delete=models.CASCADE, related_name='answer')
 
